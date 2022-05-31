285f33a76af18dfa4bdeefdd85c3f6a7e2d59d18
dammyGideon <ajayegideon@gmail.com>
2022-05-31 11:51:24 +0100

    @fix

--- config/database.php
+++ config/database.php
@@ -46,11 +46,12 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'email_signature'),
+            'username' => env('DB_USERNAME', 'gravesfoods'),
+            'password' => env('DB_PASSWORD', '2022ITSD!
+            '),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
